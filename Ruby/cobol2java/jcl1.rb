a795c5321d8b137f7a4cf96528631e23c208faa0
german <german.eric@gmail.com>
2014-04-18 22:45:32 +0200

    parse jcl

---
+++ Ruby/cobol2java/jcl1.rb
@@ -0,0 +1,32 @@
+require 'json'
+########## lecture des jcl IBM Z/OS
+# extraction du nom de jcl 
+r1  = /\/\/(.+?)\s/  
+# extraction du nom des programmes du jcl
+r2  = /PGM=(VFP.+?)\s/ 
+
+Dir.glob("jcl/*.txt").each do |filename| 
+ f= File.open(filename)
+@tabpgm = []
+hash={}
+content = File.readlines(f)
+content.each_with_index do |ligne,i|
+    l2 = ligne.force_encoding("UTF-8")
+    if i == 0  then 
+
+        tjcl =  l2.match r1 
+        @jcl = tjcl[1] 
+    end 
+    begin
+    tpgm = ligne.match r2
+    rescue
+    end   
+    if tpgm then 
+          @tabpgm << tpgm[1]
+     end
+ end
+hash[:jcl] = @jcl
+hash[:pgm] = @tabpgm
+
+puts hash.to_json
+end      
