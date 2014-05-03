e3c1b8488d91078899e5f0663b92b32908222833
german <german.eric@gmail.com>
2014-05-03 21:40:37 +0200

    exemple commande montage

---
+++ Ruby/experiences/concatjpeg.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+#ce script concatene deux images, les grossisant deux fois. 
+#il ajoute un titre de taille 8 
+
+montage 9692950127_8a56fd905c_n.jpg 9692954315_8d8febd6f2_n.jpg  -geometry +2+2  -title "J'espere du wifi quand je vois ce panneau" -pointsize 8  essai.jpg
