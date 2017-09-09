17f5770ea91ce74e4674c036fc88655238e69822
Eric German <german.eric@gmail.com>
2017-09-09 14:04:47 +0200

    labs blockchain

---
+++ blockchain/Blockchain.mm
@@ -0,0 +1,286 @@
+<map version="freeplane 1.3.0">
+<!--To view this file, download free mind mapping software Freeplane from http://freeplane.sourceforge.net -->
+<node TEXT="Blockchain" ID="ID_1723255651" CREATED="1283093380553" MODIFIED="1504424521384"><hook NAME="MapStyle">
+
+<map_styles>
+<stylenode LOCALIZED_TEXT="styles.root_node">
+<stylenode LOCALIZED_TEXT="styles.predefined" POSITION="right">
+<stylenode LOCALIZED_TEXT="default" MAX_WIDTH="600" COLOR="#000000" STYLE="as_parent">
+<font NAME="SansSerif" SIZE="10" BOLD="false" ITALIC="false"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="defaultstyle.details"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.note"/>
+<stylenode LOCALIZED_TEXT="defaultstyle.floating">
+<edge STYLE="hide_edge"/>
+<cloud COLOR="#f0f0f0" SHAPE="ROUND_RECT"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.user-defined" POSITION="right">
+<stylenode LOCALIZED_TEXT="styles.topic" COLOR="#18898b" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subtopic" COLOR="#cc3300" STYLE="fork">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.subsubtopic" COLOR="#669900">
+<font NAME="Liberation Sans" SIZE="10" BOLD="true"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.important">
+<icon BUILTIN="yes"/>
+</stylenode>
+</stylenode>
+<stylenode LOCALIZED_TEXT="styles.AutomaticLayout" POSITION="right">
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level.root" COLOR="#000000">
+<font SIZE="18"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,1" COLOR="#0033ff">
+<font SIZE="16"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,2" COLOR="#00b439">
+<font SIZE="14"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,3" COLOR="#990000">
+<font SIZE="12"/>
+</stylenode>
+<stylenode LOCALIZED_TEXT="AutomaticLayout.level,4" COLOR="#111111">
+<font SIZE="10"/>
+</stylenode>
+</stylenode>
+</stylenode>
+</map_styles>
+</hook>
+<hook NAME="AutomaticEdgeColor" COUNTER="6"/>
+<node TEXT="Origines" POSITION="right" ID="ID_1666530097" CREATED="1504424524114" MODIFIED="1504687432161">
+<icon BUILTIN="idea"/>
+<icon BUILTIN="wizard"/>
+<edge COLOR="#ff0000"/>
+<node TEXT="1990" OBJECT="java.lang.Long|1990" ID="ID_389288822" CREATED="1504424599529" MODIFIED="1504424607310"/>
+<node TEXT="La mouvence des cypherpunks" ID="ID_1342387351" CREATED="1504596598440" MODIFIED="1504613892476"/>
+<node TEXT="Les crises financi&#xe8;res" ID="ID_756810412" CREATED="1504596615292" MODIFIED="1504596622091">
+<node TEXT="Subprimes 2008" ID="ID_1658983113" CREATED="1504685339975" MODIFIED="1504685544795"/>
+<node TEXT="Grece/Chypre  2012" ID="ID_1866945121" CREATED="1504685336528" MODIFIED="1504685496177"/>
+</node>
+<node TEXT="Bitcoin" ID="ID_1711614737" CREATED="1504424607893" MODIFIED="1504424616630">
+<node TEXT="satoshi  nakamoto" ID="ID_917253363" CREATED="1504615680885" MODIFIED="1504616552866"/>
+<node TEXT="21 million de bitcoin" ID="ID_271176658" CREATED="1504615698397" MODIFIED="1504615709399">
+<node TEXT="en 2141 apres 33 divisions" ID="ID_898515008" CREATED="1504690944120" MODIFIED="1504690953725"/>
+<node TEXT="division tous les 210000 blocs" ID="ID_759325694" CREATED="1504691055086" MODIFIED="1504691099140"/>
+<node TEXT="prime 12,5 BTC jusqu&apos;a 2020/ bloc 315 millions de $ de primes par an" ID="ID_249622968" CREATED="1504690988869" MODIFIED="1504691124173"/>
+</node>
+</node>
+<node TEXT="Ethereum" ID="ID_1987575702" CREATED="1504613476750" MODIFIED="1504613534860"/>
+<node TEXT="600 monnaies crytographiques" ID="ID_151357437" CREATED="1504686190021" MODIFIED="1504686204802"/>
+</node>
+<node TEXT="Techniques" POSITION="right" ID="ID_1448890168" CREATED="1504424534362" MODIFIED="1504687363929">
+<icon BUILTIN="launch"/>
+<edge COLOR="#0000ff"/>
+<node TEXT="base de donn&#xe9;es decentralis&#xe9;e" ID="ID_957111545" CREATED="1504534204294" MODIFIED="1504687464768">
+<icon BUILTIN="group"/>
+<node TEXT="pas de serveur maitre" ID="ID_1419595733" CREATED="1504686397386" MODIFIED="1504686423144"/>
+<node TEXT="chainage par la &apos;marque&apos; numerique du bloc precedent" ID="ID_150383467" CREATED="1504539147716" MODIFIED="1504539217789"/>
+<node TEXT="cartographie de CAP" ID="ID_591451373" CREATED="1504686358023" MODIFIED="1504686367190">
+<node TEXT="de type eventuellement consistante" ID="ID_678367192" CREATED="1504686250541" MODIFIED="1504686350465"/>
+<node TEXT="La stabilit&#xe9; est proportionnelle &#xe0; la profondeur" ID="ID_1232976980" CREATED="1504613564667" MODIFIED="1504613634243"/>
+</node>
+</node>
+<node TEXT="r&#xe9;seau P2P" ID="ID_652573086" CREATED="1504534223981" MODIFIED="1504687480170">
+<icon BUILTIN="mindmap"/>
+<node TEXT="protocole de decouverte" ID="ID_1805955909" CREATED="1504613660398" MODIFIED="1504613667326">
+<node TEXT="DNS &apos;seed&apos;" ID="ID_579535638" CREATED="1504615190368" MODIFIED="1504615197559"/>
+</node>
+<node TEXT="protocole de synchronisation" ID="ID_1652551360" CREATED="1504613668015" MODIFIED="1504613679968"/>
+<node TEXT="d&#xe9;cisions prises &#xe0; la  majorit&#xe9; des noeuds" ID="ID_1143281824" CREATED="1504613753782" MODIFIED="1504613806626">
+<node TEXT="Th&#xe9;rorie des jeux: les g&#xe9;n&#xe9;raux bizantins" ID="ID_1435201363" CREATED="1504686608556" MODIFIED="1504686630821"/>
+</node>
+<node TEXT="noeuds" ID="ID_755748675" CREATED="1504614397435" MODIFIED="1504614409066">
+<node TEXT="full node" ID="ID_665688556" CREATED="1504614410391" MODIFIED="1504614414826">
+<node TEXT="entre 5000 &#xe0; 8000 serveurs" ID="ID_1028290978" CREATED="1504614534930" MODIFIED="1504614552740">
+<node TEXT="Env 100 Go" ID="ID_1350340981" CREATED="1504615625053" MODIFIED="1504615632272"/>
+<node TEXT="env 2 jours d&apos;initialisation" ID="ID_112582294" CREATED="1504615633297" MODIFIED="1504686664692"/>
+</node>
+</node>
+<node TEXT="SPV (simplified payment verification)" ID="ID_199692074" CREATED="1504614415401" MODIFIED="1504614614747">
+<node TEXT="chargement limit&#xe9; aux entetes" ID="ID_1125639310" CREATED="1504616536907" MODIFIED="1504616577945"/>
+<node TEXT="charge un bloc &#xe0; la demande" ID="ID_469868007" CREATED="1504616593462" MODIFIED="1504616605081"/>
+</node>
+</node>
+<node TEXT="les pools des mineurs" ID="ID_825734969" CREATED="1504687962473" MODIFIED="1504687997076">
+<icon BUILTIN="messagebox_warning"/>
+</node>
+<node TEXT="optimisation du routage entre noeud" ID="ID_1666084281" CREATED="1504615086844" MODIFIED="1504615106923"/>
+</node>
+<node TEXT="opensource" ID="ID_1743503677" CREATED="1504534234308" MODIFIED="1504942253708">
+<icon BUILTIN="penguin"/>
+</node>
+<node TEXT="Cryptographie" ID="ID_1990211414" CREATED="1504534849040" MODIFIED="1504534854696">
+<node TEXT="cryptage asymetrique" ID="ID_1184104253" CREATED="1504539218588" MODIFIED="1504539242789">
+<node TEXT="cl&#xe9; priv&#xe9;e" ID="ID_615265228" CREATED="1504539437694" MODIFIED="1504539456602"/>
+<node TEXT="cl&#xe9; publique" ID="ID_1073271558" CREATED="1504539443420" MODIFIED="1504539461880"/>
+<node TEXT="empreinte" ID="ID_745475070" CREATED="1504539462426" MODIFIED="1504539465356">
+<node TEXT="SHA256" ID="ID_538784621" CREATED="1504799693912" MODIFIED="1504799697507"/>
+</node>
+<node TEXT="porte cl&#xe9; deterministe" ID="ID_808929356" CREATED="1504693192987" MODIFIED="1504693229992"/>
+</node>
+<node TEXT="Elliptic Curve Digital Signature Algorithm or ECDSA" ID="ID_927217723" CREATED="1504692312079" MODIFIED="1504692315260"/>
+<node TEXT="adresse d&apos;une transaction" ID="ID_908225992" CREATED="1504613851329" MODIFIED="1504613870195"/>
+</node>
+</node>
+<node TEXT="Fonctionnel" POSITION="right" ID="ID_1544314353" CREATED="1504424544034" MODIFIED="1504687386843">
+<icon BUILTIN="gohome"/>
+<edge COLOR="#00ff00"/>
+<node TEXT="pas d&apos;organe central  de r&#xe9;gulation" ID="ID_1330667137" CREATED="1504534267724" MODIFIED="1504613341297"/>
+<node TEXT="registre public de bloc de transaction, en lecture pour tous" ID="ID_755807467" CREATED="1504534282636" MODIFIED="1504539143563"/>
+<node TEXT="infalsifiable et irreversible" ID="ID_897719805" CREATED="1504534327838" MODIFIED="1504613221112">
+<node TEXT="validation d un nouveau bloc" ID="ID_429333297" CREATED="1504539741351" MODIFIED="1504942054383">
+<icon BUILTIN="button_ok"/>
+<node TEXT="v&#xe9;rification des transactions" ID="ID_1257808195" CREATED="1504539783076" MODIFIED="1504942047687">
+<icon BUILTIN="go"/>
+</node>
+<node TEXT="ressoudre une enigme math&#xe9;matique" ID="ID_1915649238" CREATED="1504539794579" MODIFIED="1504539814682"/>
+<node TEXT="obtenir le droit d &#xe9;crire un nouveau bloc" ID="ID_573346386" CREATED="1504539815377" MODIFIED="1504539827828"/>
+</node>
+<node TEXT="d&#xe9;lais d&apos;insertion d&apos;un nouveau bloc" ID="ID_678366758" CREATED="1504613444950" MODIFIED="1504942072555">
+<icon BUILTIN="clock"/>
+<node TEXT="re-&#xe9;valuer tous les 15 jours" ID="ID_352159213" CREATED="1504616629744" MODIFIED="1504616647827"/>
+</node>
+</node>
+<node TEXT="identification" ID="ID_700915769" CREATED="1504534343628" MODIFIED="1504613291974">
+<node TEXT="pseudonyme" ID="ID_516308972" CREATED="1504942228464" MODIFIED="1504942234490"/>
+</node>
+</node>
+<node TEXT="les blocs" POSITION="right" ID="ID_253045697" CREATED="1504614856803" MODIFIED="1504690766106">
+<icon BUILTIN="xmag"/>
+<edge COLOR="#ffff00"/>
+<node TEXT="Transactions" ID="ID_242570605" CREATED="1504614868778" MODIFIED="1504614880019">
+<node TEXT="env 2000" ID="ID_966716631" CREATED="1504614884140" MODIFIED="1504614889807"/>
+</node>
+<node TEXT="un index (merkle root) des transactions" ID="ID_1079163995" CREATED="1504614906063" MODIFIED="1504691294554"/>
+<node TEXT="entete de bloc" ID="ID_1220476736" CREATED="1504614890676" MODIFIED="1504614905419">
+<node TEXT="reprend l empreinte du bloc precedent" ID="ID_276315231" CREATED="1504690879298" MODIFIED="1504690894788"/>
+</node>
+<node TEXT="validation d un bloc" ID="ID_1384431276" CREATED="1504690773615" MODIFIED="1504690778962">
+<node TEXT="validation des transactions" ID="ID_869793467" CREATED="1504690779699" MODIFIED="1504690786886"/>
+<node TEXT="confection entete" ID="ID_1278177747" CREATED="1504690787855" MODIFIED="1504690793548"/>
+<node TEXT="resolution du calcul" ID="ID_877495192" CREATED="1504690794322" MODIFIED="1504690806769">
+<node TEXT="Preuve par le travail (POW)" ID="ID_1678214461" CREATED="1504690813950" MODIFIED="1504690826185">
+<node TEXT="Preuve par l&apos;enjeux" ID="ID_1699406858" CREATED="1504942186906" MODIFIED="1504942199604">
+<icon BUILTIN="help"/>
+</node>
+</node>
+<node TEXT="hashcash" ID="ID_1293058331" CREATED="1504692716784" MODIFIED="1504692720012"/>
+<node TEXT="cible" ID="ID_1692944846" CREATED="1504692724127" MODIFIED="1504692726613"/>
+<node TEXT="nonce" ID="ID_41277946" CREATED="1504692727467" MODIFIED="1504692729903"/>
+</node>
+</node>
+<node TEXT="les blocs orphelins" ID="ID_1014967549" CREATED="1504690703180" MODIFIED="1504690709312">
+<node TEXT="block en double / fork" ID="ID_237457232" CREATED="1504690723014" MODIFIED="1504690736392"/>
+</node>
+<node TEXT="6 blocs par heure" ID="ID_422508653" CREATED="1504692735158" MODIFIED="1504692748006">
+<node TEXT="autoregul&#xe9; par la hauteur de la cible" ID="ID_543530961" CREATED="1504692767060" MODIFIED="1504692804077"/>
+</node>
+</node>
+<node TEXT="Applications et perspectives" POSITION="right" ID="ID_1179104928" CREATED="1504424554156" MODIFIED="1504687411044">
+<icon BUILTIN="idea"/>
+<edge COLOR="#ff00ff"/>
+<node TEXT="Applications" ID="ID_1040755761" CREATED="1504534483756" MODIFIED="1504534487218">
+<node TEXT="Finances" ID="ID_926267341" CREATED="1504534390465" MODIFIED="1504534393521">
+<node TEXT="monnaie crytographique" ID="ID_53570011" CREATED="1504602063601" MODIFIED="1504602075375">
+<node TEXT="Su&#xe8;de" ID="ID_1255461454" CREATED="1504766657474" MODIFIED="1504766660998"/>
+<node TEXT="pays en voie de developpement" ID="ID_1887037088" CREATED="1504766661580" MODIFIED="1504766680904"/>
+</node>
+<node TEXT="ICO et token" ID="ID_657208995" CREATED="1504602076092" MODIFIED="1504602083578"/>
+</node>
+<node TEXT="tracabilit&#xe9;" ID="ID_306908823" CREATED="1504596702191" MODIFIED="1504596706108">
+<node TEXT="m&#xe9;dicament" ID="ID_795168816" CREATED="1504596707484" MODIFIED="1504596711622"/>
+<node TEXT="droit d auteur" ID="ID_779848655" CREATED="1504596716755" MODIFIED="1504596720386"/>
+</node>
+<node TEXT="Etat -civil / cadastre" ID="ID_263607327" CREATED="1504534394765" MODIFIED="1504534602820"/>
+<node TEXT="notarial" ID="ID_1902412737" CREATED="1504945833423" MODIFIED="1504945837622">
+<node TEXT="preuves et horodatage" ID="ID_925690277" CREATED="1504945857649" MODIFIED="1504945880244" LINK="https://keeex.me/fr/produits/keeex-core/"/>
+</node>
+<node TEXT="assurances" ID="ID_1897177550" CREATED="1504534398776" MODIFIED="1504534403144"/>
+<node TEXT="Les smarts contracts" ID="ID_564320910" CREATED="1504534469254" MODIFIED="1504534475796"/>
+<node TEXT="DAO  organisation decentralis&#xe9;e autonome" ID="ID_1283992926" CREATED="1504795882639" MODIFIED="1504797894730">
+<node TEXT="TheDAO : 160 millions de $ en  4 semaines" ID="ID_1119397610" CREATED="1504797896114" MODIFIED="1504797926357"/>
+</node>
+<node TEXT="IoT" ID="ID_795648005" CREATED="1504795064568" MODIFIED="1504795111487">
+<node TEXT="slock.it" ID="ID_1880236136" CREATED="1504795112203" MODIFIED="1504946392584" LINK="https://slock.it/"/>
+<node TEXT="EoT" ID="ID_755208800" CREATED="1504795158778" MODIFIED="1504795162777"/>
+</node>
+</node>
+<node TEXT="Perspectives" ID="ID_1565128073" CREATED="1504534505659" MODIFIED="1504614030844">
+<node TEXT="remplacement des tiers de confiance/centralisateur" ID="ID_1788692744" CREATED="1504534517666" MODIFIED="1504534624124">
+<node TEXT="Uber / airBNB" ID="ID_487180573" CREATED="1504534548402" MODIFIED="1504534556123"/>
+</node>
+<node TEXT="statut des chaines" ID="ID_1674170436" CREATED="1504534641845" MODIFIED="1504942411532">
+<node TEXT="publiques" ID="ID_1736864299" CREATED="1504942291047" MODIFIED="1504942300296">
+<node TEXT="bitcoin" ID="ID_1100229250" CREATED="1504942416456" MODIFIED="1504942420313"/>
+<node TEXT="ethereum" ID="ID_300183552" CREATED="1504942420855" MODIFIED="1504942427927"/>
+</node>
+<node TEXT="priv&#xe9;es ou de consortium" ID="ID_587871544" CREATED="1504942301527" MODIFIED="1504942401724">
+<node TEXT="presse" ID="ID_1730240398" CREATED="1504946613299" MODIFIED="1504946648100" LINK="https://www.orange.com/fr/Press-Room/communiques/communiques-2015/Orange-Digital-Ventures-investit-dans-Chain-premier-fournisseur-mondial-de-solutions-technologiques-de-blockchain">
+<icon BUILTIN="pencil"/>
+</node>
+<node TEXT="exemple entreprise ORANGE" ID="ID_200601661" CREATED="1504596663001" MODIFIED="1504946655601" LINK="https://chain.com/">
+<icon BUILTIN="attach"/>
+</node>
+</node>
+<node TEXT="Sidechain" ID="ID_78818321" CREATED="1504942311207" MODIFIED="1504942471908"/>
+</node>
+<node TEXT="Gestion d&apos;identit&#xe9;" ID="ID_1578707258" CREATED="1504534574103" MODIFIED="1504534579197"/>
+<node TEXT="Pour l&apos;administration" ID="ID_1473264831" CREATED="1504534557207" MODIFIED="1504534742549">
+<node TEXT="relation avec un tiers de confiance" ID="ID_917201417" CREATED="1504534658343" MODIFIED="1504534674271"/>
+<node TEXT="en tant que tiers de confiance" ID="ID_536399808" CREATED="1504534675039" MODIFIED="1504534706539"/>
+<node TEXT="en tant que r&#xe9;gulateur" ID="ID_1274542808" CREATED="1504534694059" MODIFIED="1504534736725"/>
+<node TEXT="plan juridique" ID="ID_505952604" CREATED="1504602120745" MODIFIED="1504602130358"/>
+</node>
+</node>
+</node>
+<node TEXT="Ressources" POSITION="right" ID="ID_467722623" CREATED="1504534756047" MODIFIED="1504534759689">
+<edge COLOR="#00ffff"/>
+<node TEXT="sites" ID="ID_158206314" CREATED="1504945082652" MODIFIED="1504945089349">
+<node TEXT="Url consultation de la blockchain" ID="ID_1614854070" CREATED="1504534761351" MODIFIED="1504945131444" LINK="https://blockchain.info/"/>
+<node TEXT="blockchain en mode texte" ID="ID_502897600" CREATED="1504945135573" MODIFIED="1504945212780" LINK="https://bitcoinstrings.com/"/>
+<node TEXT="autres exploreurs" ID="ID_249281737" CREATED="1504946082670" MODIFIED="1504946088195">
+<node TEXT="blockcypher" ID="ID_1839945623" CREATED="1504946089442" MODIFIED="1504946110050" LINK="https://live.blockcypher.com/"/>
+</node>
+<node TEXT="diverses surprises dans la blockchain" ID="ID_1789749395" CREATED="1504945214145" MODIFIED="1504945396501" LINK="http://www.righto.com/2014/02/ascii-bernanke-wikileaks-photographs.html">
+<node TEXT="images" ID="ID_1781543431" CREATED="1504945226169" MODIFIED="1504945289729">
+<node TEXT="" ID="ID_1722126869" CREATED="1504945352763" MODIFIED="1504945365989">
+<hook URI="Desktop/Capture%20d&#x2019;&#xe9;cran%202017-09-09%20&#xe0;%2010.21.16.png" SIZE="0.63965887" NAME="ExternalObject"/>
+</node>
+</node>
+<node TEXT="wikilealks" ID="ID_1344862901" CREATED="1504945405716" MODIFIED="1504945468703"/>
+<node TEXT="PDF, ascii art" ID="ID_1948021124" CREATED="1504945782352" MODIFIED="1504945791045"/>
+<node TEXT="adresses fausses" ID="ID_1995938485" CREATED="1504945471280" MODIFIED="1504945481130">
+<node TEXT="" ID="ID_1018920084" CREATED="1504945674659" MODIFIED="1504945727719">
+<hook URI="Desktop/Capture%20d&#x2019;&#xe9;cran%202017-09-09%20&#xe0;%2010.28.14.png" SIZE="0.51282054" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="La blockchain en france" ID="ID_812731956" CREATED="1504534794443" MODIFIED="1504534808389"/>
+<node TEXT="le projet openchain" ID="ID_94716273" CREATED="1504596565214" MODIFIED="1504941718424" LINK="ww.openchain.org"/>
+<node TEXT="ouvrages" ID="ID_1817716831" CREATED="1504534773756" MODIFIED="1504534777764">
+<node TEXT="" ID="ID_1404881418" CREATED="1504941882642" MODIFIED="1504941923361">
+<hook URI="Desktop/Capture%20d&#x2019;&#xe9;cran%202017-09-09%20&#xe0;%2009.24.29.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="" ID="ID_1165175020" CREATED="1504941979471" MODIFIED="1504942009010">
+<hook URI="Desktop/Capture%20d&#x2019;&#xe9;cran%202017-09-09%20&#xe0;%2009.26.09.png" SIZE="1.0" NAME="ExternalObject"/>
+<node TEXT="" ID="ID_624689665" CREATED="1504942532525" MODIFIED="1504942550679">
+<hook URI="Desktop/Capture%20d&#x2019;&#xe9;cran%202017-09-09%20&#xe0;%2009.35.13.png" SIZE="1.0" NAME="ExternalObject"/>
+</node>
+</node>
+</node>
+</node>
+<node TEXT="portefeuilles" ID="ID_833258756" CREATED="1504941582296" MODIFIED="1504941586579">
+<node TEXT="mycelium (btc)" ID="ID_1852403354" CREATED="1504941587411" MODIFIED="1504941594019"/>
+<node TEXT="lunar wallet (eth)" ID="ID_1494335018" CREATED="1504941594702" MODIFIED="1504941610418"/>
+</node>
+<node TEXT="change" ID="ID_350797045" CREATED="1504941629133" MODIFIED="1504941634611">
+<node TEXT="localbitcoin" ID="ID_1302615125" CREATED="1504941639109" MODIFIED="1504941754501" LINK="https://localbitcoins.com/"/>
+<node TEXT="changefly" ID="ID_1906251899" CREATED="1504941646795" MODIFIED="1504941792885" LINK="https://changefly.com/"/>
+<node TEXT="la maison du bitcoin" ID="ID_703562152" CREATED="1504941653544" MODIFIED="1504941659854"/>
+</node>
+</node>
+</node>
+</map>
