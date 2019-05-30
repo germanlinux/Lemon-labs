979f2fac7b9175ffa81e1f215c88c5833fa116d5
Eric German <german.eric@gmail.com>
2019-05-30 17:02:11 +0200

    essai de sudoku

---
+++ Python/sudoku2.py
@@ -0,0 +1,227 @@
+entree_facile = ([9, 0, 6, 4, 0, 5, 0, 8, 0],
+                 [0, 7, 2, 0, 0, 0, 0, 4, 1],
+                 [3, 4, 0, 0, 0, 2, 0, 0, 0],
+                 [0, 0, 4, 1, 6, 0, 3, 0, 0],
+                 [7, 0, 1, 0, 4, 0, 9, 0, 2],
+                 [0, 0, 5, 0, 3, 7, 4, 0, 0],
+                 [0, 0, 0, 7, 0, 0, 0, 2, 9],
+                 [8, 2, 0, 0, 0, 0, 1, 7, 0],
+                 [0, 5, 0, 9, 0, 6, 8, 0, 4])
+
+entree_moyen =  ([0, 0, 0, 0, 0, 0, 9, 1, 8],
+                 [9, 8, 4, 0, 7, 0, 0, 0, 0],
+                 [0, 2, 0, 3, 9, 0, 0, 0, 0],
+                 [3, 0, 0, 0, 8, 7, 0, 9, 6],
+                 [6, 0, 5, 0, 0, 0, 4, 0, 0],
+                 [8, 0, 0, 0, 3, 6, 0, 5, 1],
+                 [0, 1, 0, 2, 6, 0, 0, 0, 0],
+                 [4, 3, 8, 0, 1, 0, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 1, 4, 9],)
+
+entree_expert =  ([7, 1, 0, 0, 0, 0, 0, 3, 0],
+                 [0, 0, 0, 8, 0, 9, 0, 0, 1],
+                 [0, 0, 0, 0, 0, 0, 7, 0, 5],
+                 [0, 0, 4, 0, 8, 3, 0, 0, 0],
+                 [0, 0, 6, 0, 0, 0, 0, 1, 2],
+                 [0, 0, 9, 0, 5, 2, 0, 0, 0],
+                 [0, 0, 0, 0, 0, 0, 4, 0, 6],
+                 [0, 0, 0, 2, 0, 5, 0, 0, 8],
+                 [3, 2, 0, 0, 0, 0, 0, 5, 0])
+
+import copy
+
+from collections import Counter
+class Carre():
+    @staticmethod
+    def Localise(i,j):
+        adresse = {}
+        if i < 3:
+           adresse = {0, 1 ,2 }
+        elif i < 6:
+           adresse = {3, 4 ,5 } 
+        else:
+           adresse = {6, 7 ,8 }   
+        if j < 3 :
+           return adresse.intersection({0, 3, 6})
+        if j < 6 :
+           return adresse.intersection({1, 4, 7})
+        return  adresse.intersection({2, 5, 8})  
+
+
+class Grille:
+    def __init__(self, grille):
+        self.grille = list(grille) 
+        self.etat = 0
+    def get_ligne(self, nb):
+        return self.grille[nb]
+    def get_colonne(self, nb):
+        col =[]    
+        for i in range(9):    
+            col.append(self.grille[i][nb])    
+        return col    
+    def  get_carre(self, nb):
+        col =[]   
+        
+        if nb in [0,1,2] :
+            for i in range(3): 
+              col +=  self.get_ligne(i)[0+(3*nb) :3 + (3*nb) ]
+       
+        if nb in [3,4,5] :
+            for i in range(3,6): 
+              col +=  self.get_ligne(i)[0+(3*(nb-3)) :3 + (3*(nb-3))] 
+        if nb in [6,7,8] :
+            for i in range(6,9): 
+              col +=  self.get_ligne(i)[0+(3*(nb-6)) :3 + (3*(nb-6))]      
+
+        return col
+    def get_set_ligne(self, nb):
+        return(set(self.get_ligne(nb)) - {0}) 
+
+    def get_set_colonne(self, nb):
+        return(set(self.get_colonne(nb))- {0} ) 
+
+    def get_set_carre(self, nb):
+        return(set(self.get_carre(nb)) - {0} ) 
+    def affiche(self):
+        for i in range(9):
+           if i % 3 == 0:
+              print(' ') 
+           for j in range(9) :
+                if j %3 == 0:
+                   print(' ',end ='') 
+                print(self.grille[i][j],end='')
+           print(' ') 
+
+        
+from collections.abc import MutableSequence
+
+class Grilles(MutableSequence):
+    def __init__(self, grille):
+        self.grille =  [grille] 
+        super().__init__()
+
+    def __getitem__(self, i):
+        return self.grille[i]
+
+    def __len__(self):
+        return len(self.grille)
+
+    def  __setitem__(self,i, valeur):
+        self.grille[i] = valeur
+
+    def __delitem__(self, index):
+                del self.grille[index]
+
+    def insert(self, index, value):
+                self.grille.insert(index, value)
+
+
+Grilles.register(list)
+
+
+class Resolve:
+    def __init__(self, histo, etat):
+        #self.grille_depart = copy.deepcopy(histo[etat].grille)
+#        self.list_etat = [copy.deepcopy(grille)]
+        self.grille_depart = copy.deepcopy(histo[etat])
+        self.univers_possible ={}
+    def coup_simple(self):
+        cpcoup = 0
+        #
+        for i in range(9):
+            for j in range(9):
+                if self.grille_depart.grille[i][j] == 0: 
+                    possible = set(range(1,10))
+                    possible = possible - self.grille_depart.get_set_ligne(i)
+ #                   print(possible)
+                    possible = possible - set(self.grille_depart.get_set_colonne(j))
+ #                   print(possible)
+                    a = Carre.Localise(i,j)
+                    cube= a.pop()
+                    #print('cube', cube)
+                    possible = possible -  set(self.grille_depart.get_set_carre(cube))
+                    #determiner univers des possibles
+                    #print(possible)
+                    #a = i
+                    #b = j 
+                    self.univers_possible[(cube,i,j)] = possible    
+                    if len(possible) == 1:
+                        nb = possible.pop()
+                        print(f"Ligne : {i+1} Colonne : {j+1}: mettre : {nb}")
+                        cpcoup +=1
+                        self.majlignecolonne(i, j,nb, cube )
+#        for a in self.list_etat[0].lignes:
+#          print(a.valeur)
+        print('coup simple:', cpcoup)  
+        return(cpcoup)
+    def _helper_search(self, cube, nb):
+        for item, valeur in self.univers_possible.items():
+            if item[0] == cube :
+                if nb in valeur:
+                    return (item[1], item[2]) 
+                    
+    def _helper_reduction(self, cube,compteur):
+        cube = cube
+        lst_ajouer = []
+        for nb, quant in compteur.items():
+            if quant == 1 :
+                ## recherhcer coordonnée
+                l,c =self. _helper_search(cube, nb) 
+                lst_ajouer.append((cube, l, c, nb) )
+        return lst_ajouer       
+
+    def combo(self):
+        a = b = 0
+        a  =self.coup_simple()
+        if a == 0:
+            b = self.reduction_carre()
+        if a == 0 and b == 0: 
+            return False
+        return True
+              
+    def reduction_carre(self):
+        reduc = {}
+        listajouer =[]
+        for i, messet in self.univers_possible.items():
+            cle = i[0]
+            if cle in reduc:
+                reduc[cle].append(list(messet))
+            else:
+                reduc[cle] = [list(messet)]
+        for i in reduc:
+            if len(reduc[i]) > 0 :
+                compt = Counter()
+                for lst in reduc[i]:
+                   compt.update(lst)
+                a = self._helper_reduction(i,compt)
+                if len(a) > 0:
+                    listajouer.extend([*a])
+      
+        for  item in listajouer:
+            print(f"Ligne : {item[1]+1} Colonne : {item[2]+1}: mettre : {item[3]}")
+            self.majlignecolonne(item[1],item[2], item[3], item[0])
+        print('coup par reduction:',len(listajouer))  
+        print('-------------------------------------------------')
+        return(len(listajouer)) 
+
+    def majlignecolonne(self, i, j , nb , cube):
+        self.grille_depart.grille[i][j] = nb
+         
+    def etat_suivant(self):
+        return self.grille_depart
+
+                     
+
+
+grille = Grille(entree_facile)
+histogrille = Grilles(grille)
+histogrille[0].affiche()
+a = True
+cp = 0
+while a ==True:
+    cp +=1
+    moteur = Resolve(histogrille,-1)
+    a = moteur.combo()    
+    histogrille.append(moteur.etat_suivant())
+    histogrille[-1].affiche()
+print(f"resolu en {cp} coups")
---
+++ Python/sudokusaisie.py
@@ -0,0 +1,48 @@
+import pickle
+
+matrice =[]
+list_totale =[]
+with  open('mypicklefile', 'rb') as pers:
+    list_totale = pickle.load(pers)
+print(list_totale)
+
+classe = input('niveau: ')
+
+for i in range(9):
+    ligne= []
+    cpj =0
+    while cpj < 9 :
+      print(ligne)
+      a  =input()
+      if a == "":
+        a ='0'
+      for li in a:
+        ligne.append(int(li))
+        print('ajout', li) 
+        cpj +=1     
+      
+    matrice.append(ligne)
+
+    for item in matrice:
+        print(item)
+    print(f"debut ligne: {i+2}")
+# TODO 
+# ajouter modif
+rep = input("(v)alider (nb) modifier ") 
+if rep.casefold() == 'v':   
+    list_totale.append([classe, matrice])
+    pickle.dump(list_totale, open('mypicklefile', 'wb'))
+    print('sauvegarde OK')
+else:
+   nb = int(rep) 
+   print(matrice[nb])
+   ligne =[]
+   for j in range(9):
+      print(ligne)  
+      a  =input()
+      if a == "":
+        a ='0'
+      ligne.append(int(a))      
+   matrice[nb] = ligne   
+   list_totale.append([classe, matrice])
+   pickle.dump(list_totale, open('mypicklefile', 'wb'))
