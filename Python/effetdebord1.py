d1e2b338f6bed9bca2267b379f43f0cb3da64d49
germanlinux <german.eric@gmail.com>
2020-09-04 11:34:56 +0200

    exemple effer de bord

---
+++ Python/effetdebord1.py
@@ -0,0 +1,18 @@
+''' Exemple de fonction avec effet de bord:
+    la liste par defaut est initialisee au 
+    momemt de la declaration de la fonction
+    sortie pylint:
+    effetdebord1.py:6:0: W0102: Dangerous default value [] as argument (dangerous-default-value)
+
+'''
+
+def fonct(valeur, a=[]):
+    a.append(valeur)
+    return(a)
+
+res = fonct(4)
+print(res)                   # [4]
+res = fonct(5, [1, 2, 3, 4]) 
+print(res)                   # [1, 2, 3, 4, 5]
+res = fonct(4)
+print(res)                   # [4, 4]  Erreur !
