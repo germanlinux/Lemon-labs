cc5592374894a93cf555976d0454c9c877016c1d
Eric German <german.eric@gmail.com>
2021-08-31 18:01:34 +0200

    maj repo

--- Python/sudoku2.py
+++ Python/sudoku2.py
@@ -227,7 +227,12 @@ args = parser.parse_args()
 #print(args) 
 magrille = vars()['entree_' + args.grille]          
 
-#magrille = list_totale[-1][1]
+magrille = list_totale[-1][1]
+
+magrille = ([[8, 6, 0, 9, 5, 7, 3, 2, 1], [1, 9, 5, 4, 2, 3, 7, 6, 8], [7, 3, 4, 6, 1, 0, 5, 9, 0],
+ [4, 7, 1, 2, 3, 8, 0, 5, 6], [9, 8, 6, 1, 4, 5, 2, 3, 7], [3, 5, 2, 7, 6, 9, 1, 8, 4],
+ [5, 1, 8, 3, 9, 4, 6, 7, 2], [0, 2, 7, 5, 0, 1, 8, 4, 3], [6, 4, 3, 8, 7, 2, 9, 1, 5]
+])
 grille = Grille(magrille)
 histogrille = Grilles(grille)
 histogrille[0].affiche()
@@ -239,4 +244,5 @@ while encours ==True:
     encours = moteur.combo()    
     histogrille.append(moteur.etat_suivant())
     histogrille[-1].affiche()
+    print(histogrille[-1].grille)
 print(f"resolu en {cp} cycles")
